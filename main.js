91f56b73f6e6b94933c91ab500bf71481f3f8547
Misty DeRamus <mistyderamus@gmail.com>
2019-06-03 21:31:39 -0500

    more-exciting-words

---
+++ main.js
@@ -0,0 +1,21 @@
+let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+
+function addExcitement (theWordArray) {
+    let buildMeUp = ""
+
+    for (let i = 0; i < theWordArray.length; i++) {
+        /*
+            If the current value of `i` divided by 3 has no
+            remainder, add an exclamation point to the end of
+            the word and then concatenate it to `buildMeUp`.
+
+            Otherwise, just concatenate the word itself.
+         */
+
+
+        // Print buildMeUp to the console
+    }
+
+}
+
+addExcitement(sentence)
